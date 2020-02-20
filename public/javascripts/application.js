371830bf7dcff3017de6f41b152c0024a959f1ef
Ilya <freedrex@gmail.com>
2020-02-20 14:51:18 +0300

    First steps

---
+++ public/javascripts/application.js
@@ -0,0 +1,11 @@
+document.querySelector('#mix').addEventListener('click', (element) => {
+  element.preventDefault()
+
+})
+
+const add = document.querySelector('.add')
+
+add && add.addEventListener('click', (e) => {
+e.preventDefault();
+
+})
