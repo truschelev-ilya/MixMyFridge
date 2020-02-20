35a0421ee5f214e26e84828e95f72332a0b3dde9
Ilya <freedrex@gmail.com>
2020-02-20 16:28:36 +0300

    spoiler, eventlisteners, adding ingredients to form

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,31 @@
-document.querySelector('#mix').addEventListener('click', (element) => {
-  element.preventDefault()
-
-})
-
 const add = document.querySelector('.add')
+const inputList = document.querySelector('list')
+const inputs = document.querySelector(".inputs")
+const mixMyFridge = document.querySelector('#mix')
+const spoiler = document.querySelector('.spoiler')
 
-add && add.addEventListener('click', (e) => {
+
+mixMyFridge.addEventListener('click', (e) => {
 e.preventDefault();
-
+if(spoiler.style.display === '' || spoiler.style.display === 'none') {
+  spoiler.style.display = 'block'
+}else{
+  spoiler.style.display = 'none'
+  }
 })
+
+// document.querySelector('#mix').addEventListener('click', (element) => {
+//   element.preventDefault()
+
+// })
+
+add.addEventListener('click', (e) => {
+e.preventDefault();
+let completedInput = document.querySelector(".new")
+inputs.innerHTML += `<input type="text" class="form-control list" aria-describedby="button-addon2" value="${completedInput.value}" readonly>`
+completedInput.value = ""
+})
+
+if (inputList){
+
+}
