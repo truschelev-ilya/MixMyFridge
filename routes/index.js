2dfbd47ef4cd3bb41b4480ea85b763842adf9646
Ilya <freedrex@gmail.com>
2020-02-21 22:16:30 +0300

    some changes

--- routes/index.js
+++ routes/index.js
@@ -9,20 +9,24 @@ router.get('/', function (req, res, next) {
 
 router.post('/mix', async (req, res, next) => {
   const { ingredient } = await req.body //array of ingredients
-  console.log(ingredient);
   const allIngs = await ingredient.join(",");
-  console.log(allIngs);
-  
-  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${allIngs}`)
+
+  const response = await fetch(`https://www.themealdb.com/api/json/v2/9973533/filter.php?i=${allIngs}`)
   const data = await response.json()
   console.log(data);
+  const data2 = data.meals[0];
+  console.log(data2);
   
-  const meal = data.meals[0];
+  const response2 = await fetch(`https://www.themealdb.com/api/json/v2/9973533/lookup.php?i=${data2.idMeal}`)
+  const data3 = await response2.json()
+  console.log(data3);
+  const meal = data3.meals[0]
+
   res.render('find', { meal })
 })
 
 router.get('/random', async (req, res, next) => {
-  const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+  const response = await fetch('https://www.themealdb.com/api/json/v2/9973533/random.php')
   const data = await response.json()
   const meal = data.meals[0];
 
@@ -30,11 +34,11 @@ router.get('/random', async (req, res, next) => {
 })
 
 router.post('/findform', async (req, res, next) => {
-const {mealName} = req.body;
-  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
+  const { mealName } = req.body;
+  const response = await fetch(`https://www.themealdb.com/api/json/v2/9973533/search.php?s=${mealName}`)
   const data = await response.json()
   const meal = data.meals[0];
   res.render('find', { meal })
-  
+
 })
 module.exports = router;
