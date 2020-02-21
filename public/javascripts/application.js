bfa11152065b77ddf7de386497212bf2e8d4c26a
Ilya <freedrex@gmail.com>
2020-02-21 12:42:54 +0300

    random meal:hbs, fetch,css, route

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,16 +1,16 @@
 const add = document.querySelector('.add')
-const inputList = document.querySelector('list')
-const inputs = document.querySelector(".inputs")
+const list = document.querySelector('.list')
+const inputs = document.querySelector('.inputs')
 const mixMyFridge = document.querySelector('#mix')
 const spoiler = document.querySelector('.spoiler')
-
+const submitButton = document.querySelector('.submitbutton')
 
 mixMyFridge.addEventListener('click', (e) => {
-e.preventDefault();
-if(spoiler.style.display === '' || spoiler.style.display === 'none') {
-  spoiler.style.display = 'block'
-}else{
-  spoiler.style.display = 'none'
+  e.preventDefault();
+  if (spoiler.style.display === '' || spoiler.style.display === 'none') {
+    spoiler.style.display = 'block'
+  } else {
+    spoiler.style.display = 'none'
   }
 })
 
@@ -20,12 +20,38 @@ if(spoiler.style.display === '' || spoiler.style.display === 'none') {
 // })
 
 add.addEventListener('click', (e) => {
-e.preventDefault();
-let completedInput = document.querySelector(".new")
-inputs.innerHTML += `<input type="text" class="form-control list" aria-describedby="button-addon2" value="${completedInput.value}" readonly>`
-completedInput.value = ""
+  e.preventDefault();
+  let completedInput = document.querySelector(".new")
+  if(completedInput.value.length>0){
+  inputs.innerHTML += `<input type="text" name="ingredient" class="form-control list" aria-describedby="button-addon2" value="${completedInput.value}" readonly>`
+  }
+  if (list.length === 1) {
+    submitButton.innerHTML = `<input type="submit" class="btn btn-primary btn-sm" value="MiX">`
+  }
+  completedInput.value = ""
 })
 
-if (inputList){
+document.addEventListener('submit', async (e) => {
+  console.log(e.target.ingredients)
+  const response = await fetch('mix', {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+})
 
-}
+// document.querySelector('#random').addEventListener('click', async (e) => {
+//   e.preventDefault()
+
+// const response = await fetch('/random', {
+//   method: 'GET',
+//   headers: {
+//     'Content-Type': 'application/json' 
+//   }
+// })
+
+//   // const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+//   // const data = await response.json()
+//   // const meal = data.meals[0];
+// })
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/mix', (req, res, next) => {
+  const { ingredient } = req.body //array of ingredients
+  console.log(ingredient);
+
+})
+
+router.get('/random', async (req, res, next) => {
+  const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+  const data = await response.json()
+  const meal = data.meals[0];
+
+  res.render('random', { meal })
+  console.log(meal);
+})
 
 module.exports = router;
